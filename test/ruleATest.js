334aa82b0ceb0b36fd492cbbe1436ad082c80401
Ross Salge <rossnsalge@gmail.com>
2018-05-22 17:47:57 -0500

    Created four tests for ruleA, last two tests are unique to ruleA

--- test/ruleATest.js
+++ test/ruleATest.js
@@ -1,10 +1,22 @@
 const chai = require("chai");
 var assert = chai.assert;
-const rule = require("../rules/rules.js");
+const rules = require("../rules/rules.js");
 
-describe("simple function", function() {
-  it("Should return true", function() {
-    let test = rule.test();
-    assert.equal(test, true);
+describe("ruleA tests", function() {
+  it("Should be a function", function() {
+    let test = typeof rules.ruleA;
+    assert.equal(test, "function");
+  });
+  it("Should return false for invalid input", function() {
+    let res = rules.ruleA("test");
+    assert.equal(res, false);
+  });
+  it("Should return false for input that fails rule", function() {
+    let res = rules.ruleA([0, 0, 0]);
+    assert.equal(res, false);
+  });
+  it("Should return true for input that passes rule", function() {
+    let res = rules.ruleA([0, 1000]);
+    assert.equal(res, false);
   });
 });
