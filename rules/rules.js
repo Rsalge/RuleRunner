0b7fa92850ddefa37645fb25f89d9cb710947cb6
Ross Salge <rossnsalge@gmail.com>
2018-05-22 17:47:12 -0500

    setting up ruleA, ruleB, ruleC functions, just returning true currently

--- rules/rules.js
+++ rules/rules.js
@@ -1,7 +1,26 @@
-const test = () => {
+const validInput = nums => {
+  for (let i = 0; i < nums.length; i++) {
+    if (isNaN(nums[i])) {
+      console.log(
+        "Invalid Input Format, please enter only numbers followed by spaces"
+      );
+      return false;
+    }
+  }
   return true;
 };
 
+const ruleA = nums => {
+  return validInput(nums);
+};
+const ruleB = () => {
+  return true;
+};
+const ruleC = () => {
+  return true;
+};
 module.exports = {
-  test
+  ruleA,
+  ruleB,
+  ruleC
 };
