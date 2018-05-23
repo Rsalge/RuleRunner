b51410c30f9505f14f7a9735cf2d1f45d7260cd6
Ross Salge <rossnsalge@gmail.com>
2018-05-23 12:04:13 -0500

    ruleB implemented, passing all tests

--- rules/ruleB.js
+++ rules/ruleB.js
@@ -1,5 +1,15 @@
-const ruleB = () => {
-  return true;
+const ruleB = nums => {
+  let left = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    let diff = nums[i] - left;
+    if (diff >= 500) {
+      console.log("Rule RuleB passed");
+      return true;
+    }
+    left = nums[i];
+  }
+  console.log("Rule RuleB failed");
+  return false;
 };
 
 module.exports = {
--- test/ruleBTest.js
+++ test/ruleBTest.js
@@ -1,34 +1,34 @@
 const chai = require("chai");
 var assert = chai.assert;
-const rules = require("../rules/ruleB.js");
+const { ruleB } = require("../rules/ruleB.js");
 
 describe("ruleB tests", function() {
   it("Should be a function", function() {
-    let test = typeof ruleA;
+    let test = typeof ruleB;
     assert.equal(test, "function");
   });
   it("Should return false for input that fails rule", function() {
-    let res = ruleA([0, 0, 0]);
+    let res = ruleB([0, 0, 0]);
     assert.equal(res, false);
   });
   it("Should pass for a sequence that is 500 greater exactly", function() {
-    let res = ruleA([0, 500]);
+    let res = ruleB([0, 500]);
     assert.equal(res, true);
   });
   it("Should pass for input that is greater than 500 in a small increment", function() {
-    let res = ruleA([1, 501]);
+    let res = ruleB([1, 501]);
     assert.equal(res, true);
   });
   it("Should be able to handle negative values", function() {
-    let res = ruleA([-1, 499, -1]);
-    assert.equal(res, false);
+    let res = ruleB([-1, 499, -1]);
+    assert.equal(res, true);
   });
   it("Should fail for longer input containing negative and positive values", function() {
-    let res = ruleA([0, 300, -100, 399, -300, 199, -201, 100, 300, 500]);
+    let res = ruleB([0, 300, -100, 399, -300, 199, -201, 100, 300, 500]);
     assert.equal(res, false);
   });
   it("Should pass for longer input containing negative and positive values", function() {
-    let res = ruleA([
+    let res = ruleB([
       0,
       999,
       -100,
