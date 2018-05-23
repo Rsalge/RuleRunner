e541c9d96646ae11948f75c71c29b2064dfcbb08
Ross Salge <rossnsalge@gmail.com>
2018-05-23 12:11:38 -0500

    implemented ruleC, passing all tests

--- rules/ruleC.js
+++ rules/ruleC.js
@@ -1,5 +1,19 @@
-const ruleC = () => {
-  return true;
+const ruleC = nums => {
+  let avg = 0;
+  for (let i = 0; i < nums.length; i++) {
+    avg += nums[i];
+  }
+  console.log("TOTAL: ", avg);
+
+  avg /= nums.length;
+  console.log("avg: ", avg);
+
+  if (avg >= 500) {
+    console.log("Rule RuleC passed");
+    return true;
+  }
+  console.log("Rule RuleC failed");
+  return false;
 };
 
 module.exports = {
--- test/ruleCTest.js
+++ test/ruleCTest.js
@@ -1,34 +1,34 @@
 const chai = require("chai");
 var assert = chai.assert;
-const rules = require("../rules/ruleC.js");
+const { ruleC } = require("../rules/ruleC.js");
 
 describe("ruleC tests", function() {
   it("Should be a function", function() {
-    let test = typeof ruleA;
+    let test = typeof ruleC;
     assert.equal(test, "function");
   });
   it("Should return false for input that fails rule", function() {
-    let res = ruleA([0, 0, 0]);
+    let res = ruleC([0, 0, 0]);
     assert.equal(res, false);
   });
   it("Should pass for a sequence that has an average of 500 exactly", function() {
-    let res = ruleA([501, 500, 500, 499]);
+    let res = ruleC([501, 500, 500, 499]);
     assert.equal(res, true);
   });
   it("Should pass for input that has an average greater than 500 in large increments", function() {
-    let res = ruleA([0, 5000, 0, 0, 0, 0, 0, 0, 0, 0]);
+    let res = ruleC([0, 5000, 0, 0, 0, 0, 0, 0, 0, 0]);
     assert.equal(res, true);
   });
   it("Should be able to handle negative values", function() {
-    let res = ruleA([0, 1000, -2000, 4000, -500]);
-    assert.equal(res, false);
+    let res = ruleC([0, 1000, -2000, 4000, -500]);
+    assert.equal(res, true);
   });
   it("Should fail for longer input containing negative and positive values", function() {
-    let res = ruleA([0, 300, -100, 399, -300, 199, -201, 100, 300, 500]);
+    let res = ruleC([0, 300, -100, 399, -300, 199, -201, 100, 300, 500]);
     assert.equal(res, false);
   });
   it("Should pass for longer input containing negative and positive values", function() {
-    let res = ruleA([
+    let res = ruleC([
       501,
       499,
       502,
